7e9041d649c3b6224cfdbcb5459cbd33d1fc601a
Antônio Luís <antonioluisp97@gmail.com>
2023-09-27 21:46:37 -0300

    feat: class Positon

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import board.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Chess");
+		
+		System.out.println(new Position(1, 3));
 	}
 
 }
