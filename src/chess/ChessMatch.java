c2ad6f0cc940ff72962bd556123187679f69b5e2
Antônio Luís <antonioluisp97@gmail.com>
2023-09-28 23:05:39 -0300

    feat peças se movendo

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,7 @@
 package chess;
 
 import board.Board;
+import board.Piece;
 import board.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
@@ -23,6 +24,27 @@ public class ChessMatch {
 			}
 		}
 		return pieces;
+	}
+	
+	public ChessPiece performeChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+		Position source = sourcePosition.toPosition();
+		Position target = targetPosition.toPosition();
+		validateSourcePosition(source);
+		Piece capturedPiece = makeMove(source, target);
+		return (ChessPiece) capturedPiece;
+	}
+	
+	private Piece makeMove(Position source, Position target) {
+		Piece p = board.removePiece(source);
+		Piece capturedPiece = board.removePiece(target);
+		board.placePiece(p, target);
+		return capturedPiece;
+	}
+	
+	private void validateSourcePosition(Position position) {
+		if(!board.thereIsAPiece(position)) {
+			throw new ChessException("Não existe peça na posição de origem");
+		}
 	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
