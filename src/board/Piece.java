0ea180e6ba960019854371c67f6ff834791f1459
Antônio Luís <antonioluisp97@gmail.com>
2023-10-03 10:59:08 -0300

    feat movimentos possiveis para uma peça

--- src/board/Piece.java
+++ src/board/Piece.java
@@ -1,7 +1,7 @@
 package board;
 
-public class Piece {
-	
+public abstract class Piece {
+
 	protected Position position;
 	private Board board;
 
@@ -15,5 +15,23 @@ public class Piece {
 
 	protected Board getBoard() {
 		return board;
+	}
+
+	public abstract boolean[][] possibleMoves();
+
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] possibleMoves = possibleMoves();
+		for (int i = 0; i < possibleMoves.length; i++) {
+			for (int j = 0; j < possibleMoves.length; j++) {
+				if (possibleMoves[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -15,4 +15,9 @@ public class Rook extends ChessPiece {
 		return "R";
 	}
 
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] possibleMoves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return possibleMoves;
+	}
 }
